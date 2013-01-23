7a7ae818aa5edea48625d65f1246befe784fed2e
Klap-in <klapinklapin@gmail.com>
2013-01-24 00:29:36 +0100

    Rewrite to wrapper syntax. Colors can be set in config. Added background color with semicolon syntax ;;Emphasised;;. And number of colors in config set max level of emphasis degrees.

---
+++ lang/en/settings.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['colors']            = 'Font colors, ordered from low to high';
+$lang['background-colors'] = 'Background colors, ordered from low to high';
+
---
+++ lang/nl/settings.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['colors']            = 'Letterkleuren, gesorteerd van laag naar hoog';
+$lang['background-colors'] = 'Achtergrondkleuren, gesorteerd van laag naar hoog';
+
+
